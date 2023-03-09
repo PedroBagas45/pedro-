a76c5108995992b1783ad71267fa70db83d49a4d
PedroBagas45 <114693443+PedroBagas45@users.noreply.github.com>
2023-03-09 22:38:20 +0700

    Add files via upload

---
+++ identitas.java
@@ -0,0 +1,15 @@
+
+/*public class identitas{
+    public static void main(String[] args){
+        System.out.print ("A11.2021.13842 . Pedro Bagas wiraputra");
+       */
+	   public class identitas{
+    public static void main(String[] args){
+        String nama = "Pedro";
+        System.out.println("halo, nama saya adalah "+nama );
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.print("Saya sedang menulis program java");
+        
+
+    }
+}
